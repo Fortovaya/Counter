132037522320b332e81aa114bbb734d7111c7f01
Fortovaya <wilov@yandex.ru>
2024-11-04 20:47:33 +0300

    Реализована логика изменения и сброса счетчика с отображением истории

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -7,13 +7,85 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    // MARK: - @IBOutlet
+    @IBOutlet private weak var infoCountLabel: UILabel!
+    @IBOutlet private weak var infoHistoryTextView: UITextView!
+    
+    // MARK: - variables
+    
+    private var count = 0
+    
+    private lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .medium
+        formatter.locale = Locale(identifier: "ru_Ru")
+        return formatter
+    }()
+    
+    private lazy var date: Date = {
+        return Date()
+    }()
+    
+    private lazy var currentDay: String = {
+        return dateFormatter.string(from: date)
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        infoCountLabel.text = "0"
+        
+        infoHistoryTextView.text = "История изменений:\n\n"
+        infoHistoryTextView.isScrollEnabled = true
+        infoHistoryTextView.isEditable = false
     }
 
+    // MARK: - functions
+    
+    private func clear(){
+        count = 0
+        infoCountLabel.text = "0"
+        updateHistory(message: "значение сброшено")
+    }
+    
+    private func controlNegativeNumbers(){
+        if count != 0 {
+            count -= 1
+            infoCountLabel.text = "Значение счетчика: \(count)"
+            updateHistory(message: "значение изменено на -1")
+        } else {
+            updateHistory(message: "попытка уменьшить значение счётчика ниже 0") }
+    }
+    
+    private func updateHistory(message: String) {
+        let historyEntry = "\(currentDay): \(message)\n"
+        infoHistoryTextView.text.append(historyEntry)
+    }
+    
+    // MARK: - @IBAction
+    
+    @IBAction func minusCountButton(_ sender: Any) {
+        controlNegativeNumbers()
+    }
+    
+    
+    @IBAction func plusCountButton(_ sender: Any) {
+        count += 1
+        infoCountLabel.text = "Значение счетчика: \(count)"
+        updateHistory(message: "значение изменено на +1")
+    }
+    
+    
+    @IBAction func clearButton(_ sender: Any) {
+        clear()
+    }
 
+    
+    
+    
 }
 
